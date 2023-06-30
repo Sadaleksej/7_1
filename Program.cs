5ba7475b4337a798c19909f5d98acf92a5b67c65
Sadaleksej <135204154+Sadaleksej@users.noreply.github.com>
2023-06-30 14:40:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+
+Clear();
+double[,] arrr = new double[6,6];
+FilArray(arrr);
+PrintArray(arrr);
+WriteLine();
+//CompareArray(arrr);
+//PrintArray(arrr);
+//Console.WriteLine();
+//Console.WriteLine($"Сумма элементов на нечетных позициях: {SumUnEven(arrr)}");
+
+void FilArray(double[,] ar)
+{
+ Random rnd = new Random();
+    for (int i = 0; i < ar.GetLength(0); i++) 
+    {
+         for (int j = 0; j < ar.GetLength(1); j++) 
+         {
+             ar[i,j] = rnd.NextDouble() * 200-100;
+         }
+    }
+}
+
+
+
+
+void PrintArray(double[,] ar)
+{
+    for (int i = 0; i < ar.GetLength(0); i++) 
+    {
+         for (int j = 0; j < ar.GetLength(1); j++) Write(string.Format("{0:F2}", ar[i,j])+" ");
+    WriteLine();
+    }
+WriteLine();
+}
